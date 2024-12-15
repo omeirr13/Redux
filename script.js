689388e94e8a447a5dd15815f6ba043aabcaeb59
Omeir Mujtaba <omeirr13@gmail.com>
2024-12-15 12:57:27 +0500

    store responsibilities

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
 const ORDER_PIZZA = "ORDER_PIZZA";
 // we may be using action multiple times, so its better to make constants to avoid spelling mistakes.
 
@@ -12,7 +16,8 @@ const ORDER_PIZZA = "ORDER_PIZZA";
 function orderPizza() {
     return {
         type: ORDER_PIZZA,
-        shop_name: "Pizza shop"
+        payload: 90
+
     }
 }
 
@@ -28,9 +33,43 @@ const reducer = (state = initialState, action) => {
         case ORDER_PIZZA:
             return {
                 ...state,
-                pizzaBase: pizzaBase - 1
+                pizzaBase: state.pizzaBase + action.payload
             }
         default:
             return state
     }
 }
+
+
+//1st responsibility
+// Store needs to hold application state
+
+const store = createStore(reducer);
+
+//2 - it exposes a method called getStore which gives your application access to the current state in the store.
+console.log('Initial state: ', store.getState());
+
+//3 - Registers listeners via subscribe(listener)
+const unsubcribe = store.subscribe(()=>{
+    console.log("updated state", store.getState());
+});
+
+//4 - Allows state to be updated via dispatch(action)
+store.dispatch({
+    type: ORDER_PIZZA,
+    payload: 90
+})
+// store.dispatch({
+//     type: ORDER_PIZZA,
+//     payload: 90
+// })
+// store.dispatch({
+//     type: ORDER_PIZZA,
+//     payload: 90
+// })
+
+//now if we want to dispatch this multiples places, we have to update payload everywhere...so we just have a action function, single place of modification.
+
+store.dispatch(orderPizza());
+unsubcribe();
+store.dispatch(orderPizza());
